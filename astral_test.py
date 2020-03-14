ddb91ad2232c353c737e58609c6eedb6d37fed02
Peter Duerst <peterad12@gmail.com>
2020-03-13 22:42:37 -0400

    Got Home command working on Astral computer!

--- astral_test.py
+++ astral_test.py
@@ -1,13 +1,5 @@
-# https://bottlepy.org/docs/dev/tutorial.html
-
 from visca import camera
-from bottle import route, run
 
-@route('/hello')
-def hello():
-    return "Hello World!"
-
-run(host='localhost', port=8080, debug=True)
-
-# cam = camera.D30(output='COM3')
-# cam.init()
+cam = camera.D30(output='COM4')
+cam.init()
+cam.home()
--- cam_handler.py
+++ cam_handler.py
@@ -4,11 +4,14 @@ from datetime import datetime
 from visca import camera
 
 def handleCommand(cam, args):
+    command = args[0]
+    val1 = args[1]
+    val2 = args[2]
     if command == 'home':
         cam.home()
 
 # Code to execute:
-cam = camera.D30(output='COM3', deaf=1)
+cam = camera.D30(output='COM4', deaf=1)
 cam.init()
 
 args = sys.argv[1:]
