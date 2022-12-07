4193098e5c32fb4df934c2c0c8e8edfcdf686c40
Peter Duerst <peterad12@gmail.com>
2022-12-07 17:51:04 -0500

    Cleaning up, added .gitignore

--- startup.bat
+++
@@ -1,3 +0,0 @@
-ECHO OFF
-START "ASTRAL Server" /D "%~dp0app" node server.js
-START "ASTRAL Tunnel" /D "%~dp0tunnel" .\cloudflared.exe tunnel
--- startup.sh
+++ startup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # start up the tunnel from remote to localhost
-tilix -a session-add-right -t "ASTRAL Tunnel" --maximize -e "cloudflared tunnel"
+tilix -a session-add-right -t "ASTRAL Tunnel" -e "cloudflared tunnel"
 
 # start up the localhost server
-source $HOME/Workspace/common-scripts/bash-functions.sh
-set_title "ASTRAL Server"
 cd $HOME/Workspace/astral-backend/app
 node server.js
