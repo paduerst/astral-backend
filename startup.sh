4fcd1a8874ee7728973952a4ee02b9ad644d6e90
Peter Duerst <peterad12@gmail.com>
2023-07-17 19:31:31 -0400

    Added url flag to cloudflared tunnel

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start up the tunnel from remote to localhost
-tilix -a session-add-right -t "ASTRAL Tunnel" -e "cloudflared tunnel"
+tilix -a session-add-right -t "ASTRAL Tunnel" -e "cloudflared tunnel --url localhost:8080/"
 
 # start up the localhost server
 cd $HOME/Workspace/astral-backend/app
