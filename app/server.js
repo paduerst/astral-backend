c6f492abbd77c12a2fb1729294fd8b0824891374
Peter Duerst <peterad12@gmail.com>
2024-01-15 09:57:05 -0500

    Formatted server.js

--- app/server.js
+++ app/server.js
@@ -1,37 +1,31 @@
-const express = require('express')
-const url = require('url');
-const { spawn } = require('child_process');
+const express = require("express");
+const url = require("url");
+const { spawn } = require("child_process");
 
-const app = express()
-const port = 8080
-const script = 'cam_handler.py';
+const app = express();
+const port = 8080;
+const script = "cam_handler.py";
 
 function runScript(arg1, arg2, arg3) {
   return new Promise((resolve, reject) => {
     const process = spawn("python", [`./${script}`, arg1, arg2, arg3]);
 
     const out = [];
-    process.stdout.on(
-      'data',
-      (data) => {
-        out.push(data.toString());
-        console.log(`[stdout] ${data}`);
-      }
-    );
+    process.stdout.on("data", (data) => {
+      out.push(data.toString());
+      console.log(`[stdout] ${data}`);
+    });
 
     const err = [];
-    process.stderr.on(
-      'data',
-      (data) => {
-        err.push(data.toString());
-        console.log(`[stderr] ${data}`);
-      }
-    );
+    process.stderr.on("data", (data) => {
+      err.push(data.toString());
+      console.log(`[stderr] ${data}`);
+    });
 
-    process.on('exit', (code, signal) => {
+    process.on("exit", (code, signal) => {
       if (code !== 0) {
-        reject(new Error(err.join('\n')));
-        return
+        reject(new Error(err.join("\n")));
+        return;
       }
       try {
         resolve(JSON.parse(out[0]));
@@ -39,7 +33,7 @@ function runScript(arg1, arg2, arg3) {
         reject(e);
       }
     });
-  })
+  });
 }
 
 const requestListener = async function (req, res) {
@@ -50,15 +44,17 @@ const requestListener = async function (req, res) {
     try {
       output = await runScript(q.command, q.val1, q.val2);
     } catch (e) {
-      console.error('Error during script execution ', e.stack);
+      console.error("Error during script execution ", e.stack);
       process.exit(1);
     }
     res.status(200).json(output);
   } else {
-    res.json({message: 'Send a valid command next time, buddy!'});
+    res.json({ message: "Send a valid command next time, buddy!" });
   }
-}
+};
 
-app.get('/', requestListener)
+app.get("/", requestListener);
 
-app.listen(port, () => console.log(`App initialized and listening on port ${port}!`))
+app.listen(port, () =>
+  console.log(`App initialized and listening on port ${port}!`)
+);
