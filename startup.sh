4f8f32591b83f49b490c97bb4d3ba140050fadeb
Peter Duerst <peterad12@gmail.com>
2024-01-15 18:05:35 -0500

    Updated env name

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 osascript -e 'tell app "Terminal" to do script "cloudflared tunnel --url localhost:8080/"'
 
 # Start up the localhost server in a new Terminal window
-osascript -e 'tell app "Terminal" to do script "cd $HOME/Workspace/astral-backend/app && source $HOME/miniconda3/bin/activate astral-backend && node server.js"'
+osascript -e 'tell app "Terminal" to do script "cd $HOME/Workspace/astral-backend/app && source $HOME/miniconda3/bin/activate astral_backend && node server.js"'
